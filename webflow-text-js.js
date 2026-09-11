d203a0561d060b33c551147567d6ec56108bf1ae
gvi342222 <gvi34@1bin.sbs>
2026-09-11 12:55:59 +0530

    Update webflow-text-js.js

--- webflow-text-js.js
+++ webflow-text-js.js
@@ -1,1 +1,2 @@
 import slick from 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
+import fancybox from 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js';
