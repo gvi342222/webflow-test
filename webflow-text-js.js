50587a3296075595b2f7abcd1a74cdb454043348
gvi342222 <gvi34@1bin.sbs>
2026-09-11 14:13:32 +0530

    Update webflow-text-js.js

--- webflow-text-js.js
+++ webflow-text-js.js
@@ -1,5 +1,3 @@
-(function () { constversion = Date.now(); document.querySelectorAll('[data-cache-bust]').forEach(el => { constattr = el.tagName === 'LINK' ? 'href' : 'src'; consturl = el.getAttribute(attr); if (!url) return; constseparator = url.includes('?') ? '&' : '?'; el.setAttribute(attr, `${url}${separator}v=${version}`); }); })();
-
 import slick from 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
 import fancybox from 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js';
 
